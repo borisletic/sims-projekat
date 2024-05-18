0ddb0791c94fa8d4db19343d2661e31239cade20
Boris <borisletic001@gmail.com>
2024-05-18 17:41:25 +0200

    Model Class Diagram and Observers

---
+++ HotelBookingApp/Observer/ISubject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelBookingApp.Observer
+{
+    public interface ISubject
+    {
+        void NotifyObservers();
+
+        void Unsubscribe(IObserver observer);
+
+        void Subscribe(IObserver observer);
+        
+        
+    }
+}
